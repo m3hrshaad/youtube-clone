d994c979657e2f46dd0ac77089c3420d831b71af
mp <mshd.p.22@gmail.com>
2023-10-22 11:39:28 +0330

    add search and tags , sub

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -83,15 +83,35 @@ export const trend = async (req, res, next) => {
 export const sub = async (req, res, next) => {
     try {
        const user = await User.findById(req.user.id)
-       const subscribedChannels = User.subscribedUsers;
+       const subscribedChannels = user.subscribedUsers;
 
-       const list = Promise.all(
-        subscribedChannels.map(channelId=>{
+       const list = await Promise.all(
+        subscribedChannels.map((channelId)=>{
             return Video.find({userId: channelId})
         })
        )
 
-       res.status(200).json(list)
+       res.status(200).json(list.flat().sort((a,b)=> b.createdAt - a.createdAt))
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const getByTag = async (req, res, next) => {
+    const tags = req.query.tags.split(",")
+    try {
+        const videos = await Video.find({tags: {$in:tags }}).limit(20)
+        res.status(200).json(videos)
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const search = async (req, res, next) => {
+    const query = req.query.q
+    try {
+        const videos = await Video.find({title: {$regex: query, $options:"i"},}).limit(40)
+        res.status(200).json(videos)
     } catch (err) {
         next(err)
     }
--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addVideo, deleteVideo, getVideo, addView, random, sub, trend, updateVideo } from '../controllers/video.js';
+import { addVideo, deleteVideo, getVideo, addView, random, sub, trend, updateVideo, getByTag, search } from '../controllers/video.js';
 import {verifyToken} from "../verifyToken.js"
 
 const router = express.Router()
@@ -13,6 +13,8 @@ router.put("/view/:id", addView);
 router.get("/trend", trend);
 router.get("/random", random);
 router.get("/sub", verifyToken, sub);
+router.get("/tags", getByTag);
+router.get("/search", search);
 
 
 export default router;
